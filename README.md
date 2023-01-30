6cc23053a0af77b1353e8033d4610013b8a84891
jfowers <jfowers@jfowers.c.groq-143208.internal>
2023-01-30 13:51:07 -0800

    Add a placeholder readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# MLAgility repo
+
+This will be home to:
+- MLAgility benchmarking corpus with many models
+- mlabench python package with benchmarking tools
+
+But for now it is still a work in progress.
--- setup.py
+++ setup.py
@@ -25,4 +25,7 @@ setup(
             "autogroq=mlabench:autogroq",
         ]
     },
+    python_requires="==3.8.*",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
 )
