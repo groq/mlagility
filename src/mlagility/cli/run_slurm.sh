6c5bd0cfc2769f5f72f67d662857fab6ebceb956
Daniel Holanda Noronha <dhnoronha@groq.com>
2023-02-08 16:55:19 -0800

    update slurm scripts

--- src/mlagility/cli/run_slurm.sh
+++ src/mlagility/cli/run_slurm.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 script="$1"
 args="$2"
+target_file_folder="$3"
 
-# Add any data caches used by third party model hubs (huggingface, torch hub, etc.)
+# shellcheck disable=SC1090
+source /net/home/"$USER"/miniconda3/etc/profile.d/conda.sh
+conda activate tracker_slurm
+export USING_SLURM="TRUE"
 export HF_DATASETS_CACHE="/net/ml-cache/huggingface"
 export TRANSFORMERS_CACHE="/net/ml-cache/huggingface"
 export HF_HOME="/net/ml-cache/huggingface"
 export XDG_CACHE_HOME="/net/ml-cache/huggingface"
 export TORCH_HOME="/net/ml-cache/torch-hub"
-
-# shellcheck source=/dev/null
-source /net/home/"$USER"/miniconda3/etc/profile.d/conda.sh
-conda activate groqit_slurm
-export USING_SLURM="TRUE"
-# shellcheck disable=SC2046
-# shellcheck disable=SC2116
+export TORCH_HUB="/net/ml-cache/torch-hub"
+export GROQFLOW_INTERNAL_FEATURES="True"
+export GROQFLOW_SKIP_SDK_CHECK="True"
+umask 002
+python login.py --key "${HUGGINGFACE_API_KEY}"
+cd "$target_file_folder" || exit
+# shellcheck disable=SC2116,SC2046
 "$script" $(echo "$args")
 conda deactivate
--- src/mlagility/cli/slurm.py
+++ src/mlagility/cli/slurm.py
@@ -3,7 +3,7 @@ import subprocess
 import pathlib
 import time
 import getpass
-from typing import List, Optional
+from typing import List
 
 
 def jobs_in_queue(job_name=None) -> List[str]:
@@ -30,32 +30,25 @@ def jobs_in_queue(job_name=None) -> List[str]:
     return output
 
 
-def job_name(build_name) -> str:
-    return f"{build_name}_benchit"
-
-
 def run(
-    cli: str,
+    groqit_script: str,
     args: str,
-    build_name: str,
-    cache_dir: str,
+    job_name: str,
+    target_file_folder: str,
 ):
     """
     Run a GroqFlow job on Slurm
 
     args:
-        cli: an executable script that knows how to take the following arguments.
-            Most of the time this will be `benchit`, however you can provide your own
+        groqit_script: an executable script that knows how to take the following arguments.
+            Most of the time this will be `groqit-util`, however you can provide your own
             custom executable.
-        args: command line arguments passed to the cli
+        args: command line arguments passed to the groqit_script
         build_name: name of the build
-        cache_dir: location of the GroqFlow build cache
 
     """
 
-    max_jobs = 30
-
-    slurm_log_file = os.path.join(cache_dir, f"{build_name}_slurm_log.txt")
+    max_jobs = 50
 
     while len(jobs_in_queue()) >= max_jobs:
         print(
@@ -65,8 +58,6 @@ def run(
         )
         time.sleep(5)
 
-    job = job_name(build_name)
-
     shell_script = os.path.join(pathlib.Path(__file__).parent.resolve(), "run_slurm.sh")
 
     slurm_command = [
@@ -74,91 +65,14 @@ def run(
         "-c",
         "1",
         "--mem=128000",
-        f"--output={slurm_log_file}",
-        f"--job-name={job}",
+        "--time=00-02:00:00",  # days-hh:mm:ss"
+        f"--job-name={job_name}",
         shell_script,
-        cli,
+        groqit_script,
         args,
+        target_file_folder,
     ]
 
-    print(f"Submitting build {build_name} to Slurm")
+    print(f"Submitting job {job_name} to Slurm")
 
     subprocess.check_call(slurm_command)
-
-
-def list_arg(values, flag):
-    if values is not None:
-        result = " ".join(values)
-        result = f" {flag} {result}"
-    else:
-        result = ""
-
-    return result
-
-
-def value_arg(value, flag):
-    if value is not None:
-        result = f" {flag} {value}"
-    else:
-        result = ""
-
-    return result
-
-
-def bool_arg(value, flag):
-    if value:
-        result = f" {flag}"
-    else:
-        result = ""
-
-    return result
-
-
-def run_benchit(
-    op: str,
-    search_dir: str,
-    script: str,
-    cache_dir: str,
-    rebuild: str,
-    compiler_flags: Optional[List[str]],
-    assembler_flags: Optional[List[str]],
-    num_chips: int,
-    groqview: bool,
-    devices: Optional[List[str]],
-    runtimes: Optional[List[str]],
-    ip: str,
-    max_depth: int,
-    analyze_only: bool,
-    build_only: bool,
-):
-
-    compiler_flags_str = list_arg(compiler_flags, "--compiler-flags")
-    assembler_flags_str = list_arg(assembler_flags, "--assembler-flags")
-
-    num_chips_str = value_arg(num_chips, "--num-chips")
-    groqview_str = bool_arg(groqview, "--groqview")
-
-    devices_str = list_arg(devices, "--devices")
-    runtimes_str = list_arg(runtimes, "--runtimes")
-
-    ip_str = value_arg(ip, "--ip")
-    max_depth_str = value_arg(max_depth, "--max-depth")
-    analyze_only_str = bool_arg(analyze_only, "--analyze-only")
-    build_only_str = bool_arg(build_only, "--build-only")
-
-    args = (
-        f"{op} -s {search_dir} -i {script} -d {cache_dir} "
-        f"--rebuild {rebuild}"
-        f"{compiler_flags_str}{assembler_flags_str}{num_chips_str}{groqview_str}"
-        f"{devices_str}{runtimes_str}{ip_str}{max_depth_str}{analyze_only_str}{build_only_str}"
-    )
-
-    # Remove the .py extension from the build name
-    build_name = script.split(".")[0]
-
-    run(
-        cli="benchit",
-        args=args,
-        build_name=build_name,
-        cache_dir=cache_dir,
-    )
