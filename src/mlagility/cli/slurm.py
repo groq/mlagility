groq/mlagility

commit 663b092abf7b5cf567c343b9d0f487ee51b2cb6a

Rework the API documentation and place the API under test (#104)

src/mlagility/cli/slurm.py
@@ -64,9 +64,9 @@ def run_benchit(
     cache_dir: str,
     search_dir: Optional[str] = None,
     rebuild: Optional[str] = None,
-    compiler_flags: Optional[List[str]] = None,
-    assembler_flags: Optional[List[str]] = None,
-    num_chips: Optional[int] = None,
+    groq_compiler_flags: Optional[List[str]] = None,
+    groq_assembler_flags: Optional[List[str]] = None,
+    groq_num_chips: Optional[int] = None,
     groqview: Optional[bool] = None,
     devices: Optional[List[str]] = None,
     max_depth: Optional[int] = None,
@@ -82,9 +82,9 @@ def run_benchit(
     cache_dir_str = value_arg(cache_dir, "--cache-dir")
     search_dir_str = value_arg(search_dir, "--search-dir")
     rebuild_str = value_arg(rebuild, "--rebuild")
-    compiler_flags_str = list_arg(compiler_flags, "--compiler-flags")
-    assembler_flags_str = list_arg(assembler_flags, "--assembler-flags")
-    num_chips_str = value_arg(num_chips, "--num-chips")
+    compiler_flags_str = list_arg(groq_compiler_flags, "--groq-compiler-flags")
+    assembler_flags_str = list_arg(groq_assembler_flags, "--groq-assembler-flags")
+    num_chips_str = value_arg(groq_num_chips, "--groq-num-chips")
     groqview_str = bool_arg(groqview, "--groqview")
     devices_str = list_arg(devices, "--devices")
     max_depth_str = value_arg(max_depth, "--max-depth")

.github/workflows/test.yml
@@ -15,23 +15,31 @@ permissions:
 jobs:
   build:
     runs-on: ubuntu-latest
+    env:
+      MLAGILITY_DEBUG: True
 
     steps:
       - uses: actions/checkout@v3
-      - name: Set up Python 3.8
-        uses: actions/setup-python@v3
+      - uses: conda-incubator/setup-miniconda@v2
         with:
-          python-version: "3.8"
+          miniconda-version: "latest"
+          activate-environment: mla
+          python-version: 3.8
       - name: Install dependencies
+        shell: bash -el {0}
         run: |
           python -m pip install --upgrade pip
-          pip install pylint
+          conda install pylint
           if [ -f setup.py ]; then pip install -e .; fi
           pip install transformers
       - name: Lint with PyLint
+        shell: bash -el {0}
         run: |
           pylint src --rcfile .pylintrc
       - name: Test with unittest
+        shell: bash -el {0}
         run: |
+          python examples/api/hello_world.py
           python test/cli.py
           python test/analysis.py
+          python test/model_api.py

examples/api/hello_world.py
@@ -1,6 +1,6 @@
 import argparse
 import torch
-from mlagility import benchit
+from mlagility import benchmark_model
 
 torch.manual_seed(0)
 
@@ -21,15 +21,28 @@ output_size = 500
 pytorch_model = SmallModel(input_size, output_size)
 inputs = {"x": torch.rand(input_size)}
 
+
 def main():
     # Define the argument parser
-    parser = argparse.ArgumentParser(description='Benchmark a PyTorch model on a specified device and backend.')
+    parser = argparse.ArgumentParser(
+        description="Benchmark a PyTorch model on a specified device and backend."
+    )
 
     # Add the arguments
-    parser.add_argument('--device', type=str, choices=['x86', 'nvidia'], 
-                        default='x86', help='The device to benchmark on (x86 or nvidia)')
-    parser.add_argument('--backend', type=str, choices=['local', 'cloud'], 
-                        default='local', help='The backend to use (local or cloud)')
+    parser.add_argument(
+        "--device",
+        type=str,
+        choices=["x86", "nvidia"],
+        default="x86",
+        help="The device to benchmark on (x86 or nvidia)",
+    )
+    parser.add_argument(
+        "--backend",
+        type=str,
+        choices=["local", "remote"],
+        default="local",
+        help="The backend to use (local or remote)",
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -43,7 +56,8 @@ def main():
 
     # Benchmark the model on the specified device and backend
     print(f"Benchmarking on {args.device} {args.backend}...")
-    benchit(pytorch_model, inputs, device=args.device, backend=args.backend)
+    benchmark_model(pytorch_model, inputs, device=args.device, backend=args.backend)
+
 
 if __name__ == "__main__":
     main()

src/mlagility/__init__.py
@@ -1,4 +1,5 @@
 from mlagility.version import __version__
 
-from .api.api import benchit
+from .api.script_api import benchmark_script
+from .api.model_api import benchmark_model
 from .cli.cli import main as benchitcli

src/mlagility/analysis/analysis.py
@@ -20,7 +20,7 @@ import mlagility.analysis.status as status
 import mlagility.analysis.util as util
 import mlagility.analysis.tf_helpers as tf_helpers
 import mlagility.common.labels as labels
-from mlagility.api.api import benchit
+from mlagility.api.model_api import benchmark_model
 import mlagility.common.filesystem as filesystem
 
 
@@ -33,16 +33,17 @@ class Action(Enum):
 @dataclasses.dataclass
 class TracerArgs:
     input: str
-    device: List[str]
+    device: str
+    backend: str
     actions: List[Action]
     lean_cache: bool
     targets: List[str]
     max_depth: int
     cache_dir: str
     rebuild: str
-    compiler_flags: List[str]
-    assembler_flags: List[str]
-    num_chips: int
+    groq_compiler_flags: List[str]
+    groq_assembler_flags: List[str]
+    groq_num_chips: int
     groqview: bool
     models_found: Dict[str, util.ModelInfo] = dataclasses.field(default_factory=dict)
     script_name: str = None
@@ -106,17 +107,18 @@ def call_benchit(
     labels.save_to_cache(cache_dir, build_name, tracer_args.labels)
 
     try:
-        perf = benchit(
+        perf = benchmark_model(
             model_info.model,
             inputs,
             device=tracer_args.device,
+            backend=tracer_args.backend,
             build_name=build_name,
             cache_dir=cache_dir,
             build_only=Action.BENCHMARK not in tracer_args.actions,
             lean_cache=tracer_args.lean_cache,
-            groq_num_chips=tracer_args.num_chips,
-            groq_compiler_flags=tracer_args.compiler_flags,
-            groq_assembler_flags=tracer_args.assembler_flags,
+            groq_num_chips=tracer_args.groq_num_chips,
+            groq_compiler_flags=tracer_args.groq_compiler_flags,
+            groq_assembler_flags=tracer_args.groq_assembler_flags,
             groqview=tracer_args.groqview,
             sequence=tracer_args.sequence,
         )

src/mlagility/analysis/status.py
@@ -9,8 +9,9 @@ def update(models_found: Dict[str, ModelInfo]) -> None:
     """
     Prints all models and submodels found
     """
+    if not os.environ.get("MLAGILITY_DEBUG"):
+        os.system("clear")
 
-    os.system("clear")
     printing.logn(
         "\nModels discovered during profiling:\n",
         c=printing.Colors.BOLD,

src/mlagility/api/execute-cpu.py
@@ -23,9 +23,7 @@ def run_ort_profile(source_onnx, num_iterations=100):
     per_iteration_latency = []
     exception = None
     sess_options = ort.SessionOptions()
-    sess_options.graph_optimization_level = (
-        ort.GraphOptimizationLevel.ORT_ENABLE_ALL
-    )
+    sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
     onnx_session = ort.InferenceSession(source_onnx, sess_options)
     sess_input = onnx_session.get_inputs()
     input_feed = _dummy_inputs(sess_input)
@@ -36,7 +34,7 @@ def run_ort_profile(source_onnx, num_iterations=100):
         start = timer()
         try:
             onnx_session.run([output_name], input_feed)
-        except Exception as e: # pylint: disable=broad-except
+        except Exception as e:  # pylint: disable=broad-except
             exception = e
         end = timer()
         iteration_latency = end - start
@@ -86,17 +84,15 @@ def dtype_ort2str(dtype_str: str):
 
 
 def run(
-    output_dir: str,
+    onnx_file: str,
+    outputs_file: str,
+    errors_file: str,
     num_iterations: int,
 ):
 
     # TODO: Handle exception for failure
-    onnx_model = f"{output_dir}/onnxmodel/model.onnx"
-    perf_result, exception = run_ort_profile(onnx_model, num_iterations)
+    perf_result, exception = run_ort_profile(onnx_file, num_iterations)
     # assert perf_result is list of int or float
-    save_ort_results(perf_result, num_iterations, exception)
-
-def save_ort_results(perf_result, num_iterations, exception):
 
     # Get CPU spec from lscpu
     cpu_info_command = "lscpu"
@@ -132,26 +128,42 @@ def save_ort_results(perf_result, num_iterations, exception):
     cpu_performance["Min Latency(ms)"] = str(min(perf_result) * 1000 / num_iterations)
     cpu_performance["Max Latency(ms)"] = str(max(perf_result) * 1000 / num_iterations)
 
-    with open(args["outputs_file"], "w", encoding="utf-8") as out_file:
+    with open(outputs_file, "w", encoding="utf-8") as out_file:
         json.dump(cpu_performance, out_file, ensure_ascii=False, indent=4)
 
-    with open(args["errors_file"], "w", encoding="utf-8") as e:
+    with open(errors_file, "w", encoding="utf-8") as e:
         e.writelines(str(exception))
+
 
 if __name__ == "__main__":
     # Parse Inputs
     parser = argparse.ArgumentParser(description="Execute models built by GroqFlow")
-    parser.add_argument("output_dir", help="Path where the build files are stored")
-    parser.add_argument("outputs_file", help="File in which the outputs will be saved")
-    parser.add_argument("errors_file", help="File in which the outputs will be saved")
+    parser.add_argument(
+        "output_dir",
+        help="Path where the build files are stored",
+    )
+    parser.add_argument(
+        "onnx_file",
+        help="Path where the ONNX file is located",
+    )
+    parser.add_argument(
+        "outputs_file",
+        help="File in which the outputs will be saved",
+    )
+    parser.add_argument(
+        "errors_file",
+        help="File in which the outputs will be saved",
+    )
     parser.add_argument(
         "iterations",
         type=int,
         help="Number of times to execute the received onnx model",
     )
-    args = vars(parser.parse_args())
+    args = parser.parse_args()
 
     run(
-        args["output_dir"],
-        args["iterations"],
+        args.onnx_file,
+        args.outputs_file,
+        args.errors_file,
+        args.iterations,
     )

commit c39f3b0cd553bc0536459c308c505dd4bfb83ea0

Fix bug caused by #104

src/mlagility/cli/slurm.py
@@ -64,9 +64,9 @@ def run_benchit(
     cache_dir: str,
     search_dir: Optional[str] = None,
     rebuild: Optional[str] = None,
-    compiler_flags: Optional[List[str]] = None,
-    assembler_flags: Optional[List[str]] = None,
-    num_chips: Optional[int] = None,
+    groq_compiler_flags: Optional[List[str]] = None,
+    groq_assembler_flags: Optional[List[str]] = None,
+    groq_num_chips: Optional[int] = None,
     groqview: Optional[bool] = None,
     devices: Optional[List[str]] = None,
     max_depth: Optional[int] = None,
@@ -82,9 +82,9 @@ def run_benchit(
     cache_dir_str = value_arg(cache_dir, "--cache-dir")
     search_dir_str = value_arg(search_dir, "--search-dir")
     rebuild_str = value_arg(rebuild, "--rebuild")
-    compiler_flags_str = list_arg(compiler_flags, "--compiler-flags")
-    assembler_flags_str = list_arg(assembler_flags, "--assembler-flags")
-    num_chips_str = value_arg(num_chips, "--num-chips")
+    compiler_flags_str = list_arg(groq_compiler_flags, "--compiler-flags")
+    assembler_flags_str = list_arg(groq_assembler_flags, "--assembler-flags")
+    num_chips_str = value_arg(groq_num_chips, "--num-chips")
     groqview_str = bool_arg(groqview, "--groqview")
     devices_str = list_arg(devices, "--devices")
     max_depth_str = value_arg(max_depth, "--max-depth")