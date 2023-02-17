097b95870d62fa1b452b9f2aea1a24057b0f8760
jfowers <jeremyfowers@outlook.com>
2023-02-17 08:49:27 -0800

    Rename execution scripts according to runtime

--- src/mlagility/api/devices.py
+++ src/mlagility/api/devices.py
@@ -9,6 +9,9 @@ import groqflow.common.exceptions as exp
 import groqflow.common.build as build
 import groqflow.common.sdk_helpers as sdk
 
+ORT_BENCHMARKING_SCRIPT = "execute_ort.py"
+TRT_BENCHMARKING_SCRIPT = "execute_trt.py"
+
 
 class BenchmarkPaths:
     def __init__(self, state, device, backend, username=None):
@@ -211,14 +214,14 @@ def setup_remote_host(client, device_type: str, output_dir: str) -> None:
         if stdout == "" or exit_code == 1:
             msg = "No NVIDIA GPUs available on the remote machine"
             raise exp.GroqModelRuntimeError(msg)
-        files_to_transfer = ["execute-gpu.py"]
+        files_to_transfer = [TRT_BENCHMARKING_SCRIPT]
     elif device_type == "x86":
         # Check if x86_64 CPU is available remotely
         stdout, exit_code = exec_command(client, "uname -i")
         if stdout != "x86_64" or exit_code == 1:
             msg = "Only x86_64 CPUs are supported at this time for benchmarking"
             raise exp.GroqModelRuntimeError(msg)
-        files_to_transfer = ["execute-cpu.py", "setup_ort_env.sh"]
+        files_to_transfer = [ORT_BENCHMARKING_SCRIPT, "setup_ort_env.sh"]
     elif device_type == "groqchip":
         # Check if at least one GroqChip Processor is available remotely
         stdout, exit_code = exec_command(client, "/usr/bin/lspci -n")
@@ -253,7 +256,7 @@ def setup_local_host(device_type: str, output_dir: str) -> None:
         if stdout != "x86_64" or check_device.returncode == 1:
             msg = "Only x86_64 CPUs are supported at this time for competitive benchmarking"
             raise exp.GroqModelRuntimeError(msg)
-        files_to_transfer = ["execute-cpu.py", "setup_ort_env.sh"]
+        files_to_transfer = [ORT_BENCHMARKING_SCRIPT, "setup_ort_env.sh"]
 
     elif device_type == "nvidia":
         # Check if at least one NVIDIA GPU is available locally
@@ -268,7 +271,7 @@ def setup_local_host(device_type: str, output_dir: str) -> None:
         if "NVIDIA" not in result.stdout or result.returncode == 1:
             msg = "No NVIDIA GPUs available on the local machine"
             raise exp.GroqModelRuntimeError(msg)
-        files_to_transfer = ["execute-gpu.py"]
+        files_to_transfer = [TRT_BENCHMARKING_SCRIPT]
 
     else:
         raise ValueError(f"Invalid device type: {device_type}")
@@ -380,7 +383,7 @@ def execute_gpu_remotely(state: build.State, device: str, iterations: int) -> No
     # Run benchmarking script
     _, exit_code = exec_command(
         client,
-        f"/usr/bin/python3 {remote_paths.output_dir}/execute-gpu.py "
+        f"/usr/bin/python3 {remote_paths.output_dir}/{TRT_BENCHMARKING_SCRIPT} "
         f"{remote_paths.output_dir} {docker_paths.onnx_file} {remote_paths.outputs_file} "
         f"{remote_paths.errors_file} {iterations}",
     )
@@ -450,7 +453,7 @@ def execute_gpu_locally(state: build.State, device: str, iterations: int) -> Non
     run_benchmark = subprocess.Popen(
         [
             python_location,
-            os.path.join(local_paths.output_dir, "execute-gpu.py"),
+            os.path.join(local_paths.output_dir, TRT_BENCHMARKING_SCRIPT),
             local_paths.output_dir,
             docker_paths.onnx_file,
             local_paths.outputs_file,
@@ -517,7 +520,7 @@ def execute_cpu_remotely(state: build.State, device: str, iterations: int) -> No
     _, exit_code = exec_command(
         client,
         f"/home/{username}/miniconda3/envs/{env_name}/bin/python "
-        f"{os.path.join(remote_paths.output_dir, 'execute-cpu.py')} "
+        f"{os.path.join(remote_paths.output_dir, ORT_BENCHMARKING_SCRIPT)} "
         f"{remote_paths.output_dir} {remote_paths.onnx_file} {remote_paths.outputs_file} "
         f"{remote_paths.errors_file} {iterations}",
     )
@@ -609,7 +612,7 @@ def execute_cpu_locally(state: build.State, device: str, iterations: int) -> Non
     run_benchmark = subprocess.Popen(
         [
             f"{conda_src}envs/{env_name}/bin/python",
-            os.path.join(local_paths.output_dir, "execute-cpu.py"),
+            os.path.join(local_paths.output_dir, ORT_BENCHMARKING_SCRIPT),
             local_paths.output_dir,
             local_paths.onnx_file,
             local_paths.outputs_file,
