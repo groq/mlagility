4d0e481012c921b899400c4aad12ae2d36db0e22
jfowers <jeremyfowers@outlook.com>
2023-02-17 11:30:57 -0800

    Add a docstring to example_sequence.py

--- examples/cli/extras/example_sequence.py
+++ examples/cli/extras/example_sequence.py
@@ -1,6 +1,21 @@
+"""
+This script is an example of a sequence.py file. Such a sequence.py file
+can be used to redefine the build phase of the benchit CLI, benchmark_script(),
+and benchmark_model() to have any custom behavior.
+
+In this example sequence.py file we are setting the build sequence to simply
+export from pytorch to ONNX. This differs from the default build sequence, which
+exports to ONNX, optimizes, and converts to float16.
+
+You can pass this file into benchit with a command like:
+
+    benchit benchmark INPUT_SCRIPT --sequence-file example_sequence.py --build-only
+"""
+
 from groqflow.justgroqit.stage import Sequence
 import groqflow.justgroqit.export as export
 from mlagility.common.groqflow_helpers import SuccessStage
+
 
 def get_sequence():
     return Sequence(
