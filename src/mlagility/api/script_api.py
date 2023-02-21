25efd0a9ecb1ee17b7168228786bb467fe9b9f4e
Daniel Holanda Noronha <dhnoronha@groq.com>
2023-02-21 11:14:27 -0800

    Revert changes - Checking for regular expressions with the previous techique for raising exception is not reliable

--- src/mlagility/api/script_api.py
+++ src/mlagility/api/script_api.py
@@ -92,33 +92,17 @@ def benchmark_script(
             "node and your local machine."
         )
 
+    # Ignore everything after the '::' symbol, if there is one
+    clean_scripts = [script.split("::")[0] for script in input_scripts]
+
     # Validate that the script exists
-    regular_expressions_metacharacters = ["?", "+", "{", "|", "(", ")"]
-    for input_script in input_scripts:
-        script = input_script.split("::")[0]
-        contains_hash = len(script.split("::")) == 2
-        is_regular_expression = any(
-            [regex_char in script for regex_char in regular_expressions_metacharacters]
-        )
+    for script in clean_scripts:
         if os.path.isdir(script):
             raise exceptions.GroqitArgError(
                 f'"{script}" is a directory. Do you mean "{script}/*.py" ?'
             )
         if not os.path.isfile(script):
-            if is_regular_expression and contains_hash:
-                raise exceptions.GroqitArgError(
-                    (
-                        "Using bash regular expressions and filtering model by hashes are "
-                        "mutually exclusive. To filter models by hashes, please provide the "
-                        "full path of the Python script rather than a regular expression."
-                    )
-                )
-            elif is_regular_expression:
-                raise exceptions.GroqitArgError(
-                    f"The regular expression {script} did not match any file"
-                )
-            else:
-                raise exceptions.GroqitArgError(f"Script {script} could not be found")
+            raise exceptions.GroqitArgError(f"Script {script} could not be found")
         if not script.endswith(".py"):
             raise exceptions.GroqitArgError(f"Script must end with .py (got {script})")
 
