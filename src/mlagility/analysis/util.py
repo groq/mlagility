71d3e97d4256cba0e573a811022a86c4a0814a23
ramkrishna2910 <ramkrishna2910@gmail.com>
2023-03-20 15:23:57 -0700

    Add tests and resolve comments

--- src/mlagility/analysis/util.py
+++ src/mlagility/analysis/util.py
@@ -1,6 +1,5 @@
 import sys
 from dataclasses import dataclass
-from collections import defaultdict
 from typing import Callable, List, Union, Dict
 import inspect
 import torch
@@ -81,48 +80,54 @@ def count_parameters(model: torch.nn.Module, model_type: build.ModelType) -> int
     # Raise exception if an unsupported model type is provided
     raise AnalysisException(f"model_type {model_type} is not supported")
 
-
 def get_onnx_ops_list(onnx_model) -> Dict:
     """
     List unique ops found in the onnx model
     """
-    onnx_ops_counter = defaultdict(int)
+    onnx_ops_counter = {}
     try:
         model = onnx.load(onnx_model)
     except Exception as e: # pylint: disable=broad-except
-        print (f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
-        return "ONNX model analysis failed"
+        printing.log_warning(f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
+        return onnx_ops_counter
     for node in model.graph.node: # pylint: disable=E1101
-        onnx_ops_counter[node.op_type] += 1
-    return dict(onnx_ops_counter)
+        onnx_ops_counter[node.op_type] = onnx_ops_counter.get(node.op_type, 0) + 1
+    return onnx_ops_counter
 
 
 def populate_onnx_model_info(onnx_model) -> Dict:
     """
     Read the model metadata to populate IR, Opset and model size
     """
+    result_dict = {
+        "ir_version": None,
+        "opset": None,
+        "size on disk (KiB)": None,
+    }
     try:
         model = onnx.load(onnx_model)
     except Exception as e: # pylint: disable=broad-except
-        print(f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
-        return "ONNX model analysis failed"
-    return {
+        printing.log_warning(f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
+        result_dict.update({"error": "ONNX model analysis failed"})
+        return result_dict
+    result_dict.update({
         "ir_version": getattr(model, "ir_version", None),
         "opset": getattr(model.opset_import[-1], "version", None), # pylint: disable=E1101
         "size on disk (KiB)": round(model.SerializeToString().__sizeof__() / 1024, 4),
-    }
+    })
+    return result_dict
 
 
 def onnx_input_dimensions(onnx_model) -> Dict:
     """
     Read model input dimensions
     """
+    input_shape = {}
     try:
         model = onnx.load(onnx_model)
     except Exception as e: # pylint: disable=broad-except
-        print(f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
-        return "ONNX model analysis failed"
-    input_shape = {}
+        printing.log_warning(f"Failed to get ONNX ops list from {onnx_model}: {str(e)}")
+        return input_shape
     for inp in model.graph.input: # pylint: disable=E1101
         shape = str(inp.type.tensor_type.shape.dim)
         input_shape[inp.name] = [int(s) for s in shape.split() if s.isdigit()]
