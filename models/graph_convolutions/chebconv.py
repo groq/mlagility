f8afbe965ce53686152c9ea2c68d48de62f8d529
ramkrishna2910 <ramkrishna2910@gmail.com>
2023-04-05 17:16:15 -0700

    add chebconv

---
+++ models/graph_convolutions/chebconv.py
@@ -0,0 +1,32 @@
+# labels: test_group::mlagility name::chebconv author::graph_convolutions
+"""
+The chebyshev spectral graph convolutional operator from the `"Convolutional
+Neural Networks on Graphs with Fast Localized Spectral Filtering"
+<https://arxiv.org/abs/1606.09375>`_ paper
+"""
+from mlagility.parser import parse
+import torch
+
+from torch_geometric.datasets import Planetoid
+from torch_geometric.nn import ChebConv
+
+dataset = Planetoid(root=".", name="Cora")
+data = dataset[0]
+edge_index_rows = 2
+
+# Parsing command-line arguments
+# k = parse(["k"])
+k=2
+
+model = ChebConv(
+    dataset.num_features, dataset.num_classes, k
+)  # k - Chebyshev filter size
+x = torch.ones(data.num_nodes, data.num_features, dtype=torch.float)
+edge_index = torch.ones(edge_index_rows, data.num_nodes, dtype=torch.long)
+inputs = {
+    "x": x,
+    "edge_index": edge_index,
+}
+
+# Call model
+model(**inputs)
