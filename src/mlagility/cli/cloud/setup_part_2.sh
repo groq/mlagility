1057438f11196e6ff9c04fc48d687291ee61a0e7
Jeremy Fowers <80718789+jeremyfowers@users.noreply.github.com>
2023-05-22 16:27:35 -0400

    Add a WIP azure cli (#298)

---
+++ src/mlagility/cli/cloud/setup_part_2.sh
@@ -0,0 +1,16 @@
+# Setup
+
+MYHOME=/home/azureuser
+CONDA=$MYHOME/miniconda3/bin/conda
+CONDA_ENV=mla
+
+# Create conda env
+
+$CONDA create -n $CONDA_ENV python=3.8 -y
+
+# Install mlagility
+
+cd $MYHOME
+git clone https://github.com/groq/mlagility.git
+$CONDA run -n $CONDA_ENV pip install -e mlagility
+$CONDA run -n $CONDA_ENV pip install -r mlagility/models/requirements.txt
