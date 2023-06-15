9da81c906448c37ad0a6aa796c44d436a2c5ae58
Daniel Holanda Noronha <dhnoronha@groq.com>
2023-06-15 13:02:23 -0700

    Update analysis CI

--- test/analysis.py
+++ test/analysis.py
@@ -220,7 +220,7 @@ class Testing(unittest.TestCase):
         output = run_analysis(
             [
                 "benchit",
-                "linear_pytorch.py::bf68fb06",
+                "linear_pytorch.py::87cea470",
                 "--max-depth",
                 "1",
                 "--build-only",
@@ -231,7 +231,7 @@ class Testing(unittest.TestCase):
         assert np.array_equal(output, (2, 0, 1))
 
     def test_05_cache(self):
-        model_hash = "bf68fb06"
+        model_hash = "87cea470"
         run_analysis(
             [
                 "benchit",
@@ -321,7 +321,7 @@ class Testing(unittest.TestCase):
         output = run_analysis(
             [
                 "benchit",
-                "linear_pytorch.py::bf68fb06",
+                "linear_pytorch.py::87cea470",
                 "--build-only",
                 "--max-depth",
                 "1",
@@ -332,7 +332,7 @@ class Testing(unittest.TestCase):
         assert np.array_equal(output, (2, 0, 1))
 
     def test_13_clean_cache(self):
-        model_hash = "bf68fb06"
+        model_hash = "87cea470"
         run_analysis(
             [
                 "benchit",
