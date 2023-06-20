c9729303d76c5f0c0434efdd5536594d84ac580e
Daniel Holanda Noronha <dhnoronha@groq.com>
2023-06-20 15:13:54 -0700

    Better UI

--- src/mlagility/analysis/status.py
+++ src/mlagility/analysis/status.py
@@ -28,28 +28,38 @@ def recursive_print(
     script_names_visited = []
 
     for model_hash in models_found.keys():
-        for workload_hash in models_found[model_hash].workloads.keys():
-            workload = models_found[model_hash].workloads[workload_hash]
+        model_visited = False
+        model_info = models_found[model_hash]
+        workload_idx = 0
+        for workload_hash in model_info.workloads.keys():
+            workload = model_info.workloads[workload_hash]
 
             if (
-                parent_model_hash == models_found[model_hash].parent_hash
+                parent_model_hash == model_info.parent_hash
                 and workload.executed > 0
                 and (
-                    models_found[model_hash].workloads[workload_hash].parent_hash
+                    model_info.workloads[workload_hash].parent_hash
                     == parent_workload_hash
                 )
             ):
-
                 print_file_name = False
-                if models_found[model_hash].script_name not in script_names_visited:
-                    script_names_visited.append(models_found[model_hash].script_name)
-                    if models_found[model_hash].depth == 0:
+                if model_info.script_name not in script_names_visited:
+                    script_names_visited.append(model_info.script_name)
+                    if model_info.depth == 0:
                         print_file_name = True
 
-                print_workload(models_found[model_hash], workload_hash, print_file_name)
+                print_workload(
+                    model_info,
+                    workload_hash,
+                    print_file_name,
+                    workload_idx=workload_idx,
+                    model_visited=model_visited,
+                )
+                model_visited = True
+                workload_idx += 1
 
                 if print_file_name:
-                    script_names_visited.append(models_found[model_hash].script_name)
+                    script_names_visited.append(model_info.script_name)
 
                 recursive_print(
                     models_found,
@@ -63,48 +73,63 @@ def print_workload(
     model_info: ModelInfo,
     workload_hash: Union[str, None],
     print_file_name: bool = False,
+    workload_idx: int = 0,
+    model_visited: bool = False,
 ) -> None:
     """
     Print information about a given model or submodel
     """
+    workload = model_info.workloads[workload_hash]
     ident = "\t" * (2 * model_info.depth + 1)
     if print_file_name:
         print(f"{model_info.script_name}.py:")
-    printing.log(f"{ident}{model_info.name} ")
 
-    # Show the number of times the model has been executed
-    # Only show the execution time if we are not running benchit() as this
-    # impacts time measurement.
-    workload = model_info.workloads[workload_hash]
     if workload.exec_time == 0 or model_info.build_model:
         exec_time = ""
     else:
         exec_time = f" - {workload.exec_time:.2f}s"
-    printing.logn(
-        f"(executed {workload.executed}x{exec_time})",
-        c=printing.Colors.OKGREEN,
-    )
 
-    if model_info.model_type == build.ModelType.PYTORCH:
-        print(f"{ident}\tModel Type:\tPytorch (torch.nn.Module)")
-    elif model_info.model_type == build.ModelType.KERAS:
-        print(f"{ident}\tModel Type:\tKeras (tf.keras.Model)")
-
-    # Display class of found model and the file where it was found
-    model_class = type(model_info.model)
-    print(f"{ident}\tClass:\t\t{model_class.__name__} ({model_class})")
     if model_info.depth == 0:
-        print(f"{ident}\tLocation:\t{model_info.file}, line {model_info.line}")
+        if not model_visited:
+            printing.logn(f"{ident}{model_info.name}")
+    else:
+        printing.log(f"{ident}{model_info.name}")
+        printing.logn(
+            f" (executed {workload.executed}x{exec_time})",
+            c=printing.Colors.OKGREEN,
+        )
 
-    # Converting number of parameters to MB assuming 2 bytes per parameter
-    model_size = model_info.params * 2 / (1024 * 1024)
-    model_size = "{:.1f}".format(model_size) if model_size > 0.1 else "<0.1"
-    print(f"{ident}\tParameters:\t{'{:,}'.format(model_info.params)} ({model_size} MB)")
-    input_shape = (
-        str(model_info.workloads[workload_hash].input_shapes)
-        .replace("{", "")
-        .replace("}", "")
-    )
+    if (model_info.depth == 0 and not model_visited) or (model_info.depth != 0):
+        if model_info.depth == 0:
+            if model_info.model_type == build.ModelType.PYTORCH:
+                print(f"{ident}\tModel Type:\tPytorch (torch.nn.Module)")
+            elif model_info.model_type == build.ModelType.KERAS:
+                print(f"{ident}\tModel Type:\tKeras (tf.keras.Model)")
+
+        # Display class of found model and the file where it was found
+        model_class = type(model_info.model)
+        print(f"{ident}\tClass:\t\t{model_class.__name__} ({model_class})")
+        if model_info.depth == 0:
+            print(f"{ident}\tLocation:\t{model_info.file}, line {model_info.line}")
+
+        # Converting number of parameters to MB assuming 2 bytes per parameter
+        model_size = model_info.params * 2 / (1024 * 1024)
+        model_size = "{:.1f}".format(model_size) if model_size > 0.1 else "<0.1"
+        print(
+            f"{ident}\tParameters:\t{'{:,}'.format(model_info.params)} ({model_size} MB)"
+        )
+
+    if model_info.depth == 0:
+        printing.logn(
+            f"\n{ident}\tWorkload {workload_idx+1} (executed {workload.executed}x{exec_time})",
+            c=printing.Colors.OKGREEN,
+        )
+
+    # Prepare input shape to be printed
+    input_shape = dict(model_info.workloads[workload_hash].input_shapes)
+    input_shape = {key: value for key, value in input_shape.items() if value != ()}
+    input_shape = str(input_shape).replace("{", "").replace("}", "")
+
     print(f"{ident}\tInput Shape:\t{input_shape}")
     print(f"{ident}\tHash:\t\t" + workload_hash)
 
@@ -143,5 +168,4 @@ def print_workload(
                     )
             else:
                 print()
-        else:
-            print("")
+        print()
